8be4d6f60ce8d1e2b90feefb4eaefed6d3e292ff
Shachar <Shachar.ratzabi.147@gmail.com>
2023-01-10 21:29:35 +0200

    disable swagger for now

--- src/main.ts
+++ src/main.ts
@@ -18,26 +18,28 @@ async function bootstrap() {
       'JWT',
     )
     .build();
-  const document = SwaggerModule.createDocument(app, swaggerConfig);
-
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const fs = require('fs');
-  fs.writeFileSync(
-    './swagger.json',
-    JSON.stringify(document).replace('"openapi":"3.0.0"', '"swagger":"2.0"'),
-  );
-
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  // const Swagger2Postman = require('swagger2-postman-generator');
-  // Swagger2Postman.convertSwagger().fromFile('swagger.json').toPostmanCollectionFile('postman_collection.json');
-
-  SwaggerModule.setup('/api/doc', app, document, {
-    customCss: 'input { max-width: unset !important; }',
-  });
+  // const document = SwaggerModule.createDocument(app, swaggerConfig);
+  //
+  // // eslint-disable-next-line @typescript-eslint/no-var-requires
+  // const fs = require('fs');
+  // fs.writeFileSync(
+  //   './swagger.json',
+  //   JSON.stringify(document).replace('"openapi":"3.0.0"', '"swagger":"2.0"'),
+  // );
+  //
+  // // eslint-disable-next-line @typescript-eslint/no-var-requires
+  // // const Swagger2Postman = require('swagger2-postman-generator');
+  // // Swagger2Postman.convertSwagger().fromFile('swagger.json').toPostmanCollectionFile('postman_collection.json');
+  //
+  // SwaggerModule.setup('/api/doc', app, document, {
+  //   customCss: 'input { max-width: unset !important; }',
+  // });
 
 
   app.use(bodyParser.json({limit: '50mb'}));
   app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
+  console.log("heorku port: ", process.env.PORT);
 
   await app.listen(process.env.PORT || 3001);
 }
