3c691436404da4ff851862360cd019dd08eb65b1
Shachar Ratzabi Mac <Shachar147@gmail.com>
2023-03-03 19:11:52 +0200

    backups

--- src/backups/backups.service.ts
+++ src/backups/backups.service.ts
@@ -4,6 +4,7 @@ import {BackupsRepository} from "./backups.repository";
 import {CreateBackupDto} from "./dto/create-backup-dto";
 import {User} from "../user/user.entity";
 import {Backups} from "./backups.entity";
+import {GetTripBackupsDto} from "./dto/get-trip-backups-dto";
 
 @Injectable()
 export class BackupsService {
@@ -14,5 +15,29 @@ export class BackupsService {
 
     async createBackup(createBackupDto: CreateBackupDto, user: User): Promise<Backups> {
         return this.backupsRepository.createBackup(createBackupDto, user)
+    }
+
+    async getTripBackups(getTripBackupsDto: GetTripBackupsDto, user: User): Promise<{
+        total: number,
+        backups: Backups[]
+    }> {
+        const data = await this.backupsRepository.find({
+            tripId: getTripBackupsDto.trip_id
+        });
+        return {
+            total: data.length,
+            backups: data
+        }
+    }
+
+    async getAllBackups(user: User): Promise<{
+        total: number,
+        backups: Backups[]
+    }> {
+        const data = await this.backupsRepository.find({});
+        return {
+            total: data.length,
+            backups: data
+        }
     }
 }
---
+++ src/backups/dto/get-trip-backups-dto.ts
@@ -0,0 +1,3 @@
+export class GetTripBackupsDto{
+    trip_id: number;
+}
