354c452450cf0f4d6ad73dbc5391591d22e957b1
Yahav Petel <yahavpetel@gmail.com>
2024-09-15 22:37:21 +0300

    wip

--- src/admin-statistics/statistics.service.ts
+++ src/admin-statistics/statistics.service.ts
@@ -14,7 +14,7 @@ export class StatisticsService {
         CROSS JOIN LATERAL jsonb_array_elements_text(json_element.value::jsonb) AS unnested_elements
       ) as a ON true
       RIGHT JOIN public.user u ON u.id = t."userId"
-      WHERE u.username != ${TEMPLATES_USER_NAME}
+      WHERE u.username != '${TEMPLATES_USER_NAME}'
       ORDER BY t."lastUpdateAt" DESC
     `;
 
