bfa5e5a88643ca6a91bdf23d00eed240bef7fb46
Yahav Petel <yahavpetel@gmail.com>
2024-09-21 11:00:57 +0300

    wip

--- src/trip-templates/trip-templates.service.ts
+++ src/trip-templates/trip-templates.service.ts
@@ -21,18 +21,18 @@ export class TripTemplatesService {
         const templatesUser = await this.userService.getUserByName(TEMPLATES_USER_NAME);
         const trips = await this.tripRepository.getTrips(filterDto, templatesUser);
 
-        // let approvedTrips = trips.filter((t) => !t.isHidden);
-        // approvedTrips = shuffle(approvedTrips);
+        let approvedTrips = trips.filter((t) => !t.isHidden);
+        approvedTrips = shuffle(approvedTrips);
 
         // temporary - return all templates, but prefer approved.
-        let approvedTrips = trips.sort((a, b) => {
-            if (a.isHidden && !b.isHidden) {
-                return 1;
-            } else if (!a.isHidden && b.isHidden) {
-                return -1;
-            }
-            return 0;
-        });
+        // let approvedTrips = trips.sort((a, b) => {
+        //     if (a.isHidden && !b.isHidden) {
+        //         return 1;
+        //     } else if (!a.isHidden && b.isHidden) {
+        //         return -1;
+        //     }
+        //     return 0;
+        // });
 
         // todo complete: add some logic of how to choose which templates to show? (prefer templates created by us? do some kind of rating mechanism for templates as well?)
 
