98a55357cccb25d7321599c8c7133b333de98701
Yahav Petel <yahavpetel@gmail.com>
2024-09-29 16:20:13 +0300

    add logs

--- src/file-upload/file-upload.controller.ts
+++ src/file-upload/file-upload.controller.ts
@@ -1,14 +1,17 @@
-import {Controller, Post, UploadedFile, UseInterceptors} from '@nestjs/common';
+import {Controller, Logger, Post, UploadedFile, UseInterceptors} from '@nestjs/common';
 import {FileInterceptor} from '@nestjs/platform-express';
 import { FileUploadService } from './file-upload.service';
 
 @Controller('file-upload')
 export class FileUploadController {
+    private logger = new Logger("FileUploadController");
+
     constructor(private readonly fileUploadService: FileUploadService) {}
 
     @Post()
     @UseInterceptors(FileInterceptor('file')) // 'file' should match the name in FormData
     async uploadFiles(@UploadedFile() file) {
+        this.logger.log('reached FileUploadController');
         const uploadedFile = await this.fileUploadService.uploadFile(file);
         return { message: 'File uploaded successfully', file: uploadedFile };
     }
--- src/file-upload/file-upload.service.ts
+++ src/file-upload/file-upload.service.ts
@@ -1,5 +1,5 @@
 import { S3Client, PutObjectCommand, ObjectCannedACL } from '@aws-sdk/client-s3';
-import { Injectable } from '@nestjs/common';
+import {Injectable, Logger} from '@nestjs/common';
 import * as path from 'path';
 import * as mime from 'mime-types';
 
@@ -8,6 +8,7 @@ import * as mime from 'mime-types';
 export class FileUploadService {
     private s3Client: S3Client;
     private bucketName = process.env.AWS_S3_BUCKET_NAME; // Set this in your .env file
+    private logger = new Logger("FileUploadController");
 
     constructor() {
         this.s3Client = new S3Client({
@@ -20,9 +21,12 @@ export class FileUploadService {
     }
 
     async uploadFile(file): Promise<string> {
+        this.logger.log('reached FileUploadService::uploadFile');
         const fileName = this.sanitizeFileName(file.originalname); // Sanitize file name
+        this.logger.log('sanitized file name');
         const fileExtension = path.extname(file.originalname); // Get the file extension
         const mimeType = mime.lookup(fileExtension); // Get MIME type
+        this.logger.log(`extracted file extension and mimeType: ${fileExtension}; ${mimeType}`);
 
         // Prepare S3 upload parameters
         const uploadParams = {
@@ -33,15 +37,27 @@ export class FileUploadService {
             // ACL: ObjectCannedACL.aws_exec_read, // Use the enum for ACL
         };
 
+        this.logger.log(`built s3 upload params: ${JSON.stringify({
+            ...uploadParams,
+            Body: '***File***'
+        })}`);
+
         try {
+            this.logger.log('Trying to upload to s3....');
+
             // Upload to S3
             const data = await this.s3Client.send(new PutObjectCommand(uploadParams));
             // console.log('File uploaded successfully:', data);
+
+            this.logger.log('Upload finished successfully!');
 
             // Return the S3 URL of the uploaded file
             return `https://${this.bucketName}.s3.${process.env.AWS_REGION}.amazonaws.com/images/pois/${fileName}`;
         } catch (error) {
             console.error('Error uploading file:', error);
+
+            this.logger.error('Upload failed :(');
+
             throw new Error('File upload failed');
         }
     }
