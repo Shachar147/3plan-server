a4f5854d50ac000fb3ae3f0a786fb06bda1ea6fe
Yahav Petel <yahavpetel@gmail.com>
2025-06-06 21:02:17 +0300

    wip

--- src/auto-schedule/auto-schedule.module.ts
+++ src/auto-schedule/auto-schedule.module.ts
@@ -2,9 +2,15 @@ import { Module } from "@nestjs/common";
 import { AutoScheduleController } from "./auto-schedule.controller";
 import { AutoScheduleService } from "./auto-schedule.service";
 import { TripModule } from "../trip/trip.module";
+import { AuthModule } from "../auth/auth.module";
+import { PassportModule } from "@nestjs/passport";
 
 @Module({
-  imports: [TripModule],
+  imports: [
+    TripModule,
+    AuthModule,
+    PassportModule,
+  ],
   controllers: [AutoScheduleController],
   providers: [AutoScheduleService],
   exports: [AutoScheduleService]
