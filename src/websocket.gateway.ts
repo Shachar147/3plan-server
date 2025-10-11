efb613598fd2e71fad6bffc2eef7bc1a848bc346
Shachar <shachar147@gmail.com>
2025-10-11 14:36:59 +0300

    wip

--- src/websocket.gateway.ts
+++ src/websocket.gateway.ts
@@ -1,107 +1,87 @@
 import { Server } from 'ws';
 import { Injectable } from '@nestjs/common';
-import * as http from "http";
-import {getRepository} from "typeorm";
-import {User} from "./user/user.entity";
-import {Trip} from "./trip/trip.entity";
+import * as http from 'http';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user/user.entity';
+import { Trip } from './trip/trip.entity';
 
-const webSocketsLogPrefix = "[WEBSOCKETS]"
+const webSocketsLogPrefix = '[WEBSOCKETS]';
 
-type WebSocketWithId = { socket: WebSocket, socketId: string};
+type WebSocketWithId = { socket: WebSocket; socketId: string };
 
 @Injectable()
 export class MyWebSocketGateway {
-    private wsServer: Server;
-    private clients: Record<number, Set<WebSocketWithId>>;
+  private wsServer: Server;
+  private clients: Record<number | string, Set<WebSocketWithId>> = {};
 
-    constructor() {
-        this.clients = {};
-    }
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepo: Repository<User>,
 
-    async init(httpServer: http.Server) {
+    @InjectRepository(Trip)
+    private readonly tripRepo: Repository<Trip>,
+  ) {}
 
-        const users = await getRepository(User).createQueryBuilder("user").getMany();
-        const trips = await getRepository(Trip).createQueryBuilder("trip").getMany();
-        const usersDict = users.reduce((acc, obj) => {
-            acc[obj.id] = obj;
-            return acc;
-        }, {});
-        const tripsDict = trips.reduce((acc, obj) => {
-            acc[obj.id] = obj;
-            return acc;
-        }, {});
+  async init(httpServer: http.Server) {
+    // ✅ Use injected repositories
+    const users = await this.userRepo.find();
+    const trips = await this.tripRepo.find();
 
-        // console.log("there", httpServer);
-        console.log(`${webSocketsLogPrefix} Initializing Websockets gateway...`);
-        this.wsServer = new Server({ server: httpServer });
-        httpServer.on('connection', (socket, req) => {
+    const usersDict = users.reduce((acc, obj) => {
+      acc[obj.id] = obj;
+      return acc;
+    }, {});
+    const tripsDict = trips.reduce((acc, obj) => {
+      acc[obj.id] = obj;
+      return acc;
+    }, {});
 
-            // Access the custom headers or query parameters from the WebSocket request
-            const userId: string = new URL(req.url, `http://${req.headers.host}`).searchParams.get('uid');
-            const socketId: string = new URL(req.url, `http://${req.headers.host}`).searchParams.get('sid');
+    console.log(`${webSocketsLogPrefix} Initializing Websockets gateway...`);
+    this.wsServer = new Server({ server: httpServer });
 
-            const tripId: string = new URL(req.url, `http://${req.headers.host}`).searchParams.get('tid');
+    httpServer.on('connection', (socket, req) => {
+      const url = new URL(req.url, `http://${req.headers.host}`);
+      const userId = url.searchParams.get('uid');
+      const socketId = url.searchParams.get('sid');
+      const tripId = url.searchParams.get('tid');
 
-            this.clients[userId] = this.clients[userId] || new Set<WebSocketWithId>();
-            this.clients[userId].add({ socket, socketId });
-            console.log(`${webSocketsLogPrefix} ${usersDict[userId]?.["username"]} (Client #${userId}) connected`, `(${this.clients[userId].size} open sessions)`);
+      this.clients[userId] = this.clients[userId] || new Set<WebSocketWithId>();
+      this.clients[userId].add({ socket, socketId });
+      console.log(`${webSocketsLogPrefix} ${usersDict[userId]?.username} (Client #${userId}) connected`, `(${this.clients[userId].size} open sessions)`);
 
-            if (tripId && tripId !== "0") {
-                this.clients[`t${tripId}`] = this.clients[`t${tripId}`] || new Set<WebSocketWithId>();
-                this.clients[`t${tripId}`].add({socket, socketId});
-                console.log(`${webSocketsLogPrefix} ${tripsDict[tripId]?.["name"]} (Trip #${tripId}) connected`, `(${this.clients['t' + tripId].size} open sessions)`);
-            }
+      if (tripId && tripId !== '0') {
+        this.clients[`t${tripId}`] = this.clients[`t${tripId}`] || new Set<WebSocketWithId>();
+        this.clients[`t${tripId}`].add({ socket, socketId });
+        console.log(`${webSocketsLogPrefix} ${tripsDict[tripId]?.name} (Trip #${tripId}) connected`, `(${this.clients['t' + tripId].size} open sessions)`);
+      }
 
-            socket.on('message', (message) => {
-                console.log(`${webSocketsLogPrefix} Received message: ${message}`);
-                // Handle WebSocket message
+      socket.on('message', (message) => {
+        console.log(`${webSocketsLogPrefix} Received message: ${message}`);
+        this.wsServer.emit('message', message);
+      });
 
-                // Broadcast the object to all connected clients
-                this.wsServer.emit('message', message);
-            });
+      socket.on('close', () => {
+        const ws1 = Array.from(this.clients[userId] || []).find((a) => a.socket == socket);
+        if (ws1) this.clients[userId].delete(ws1);
 
-            socket.on('close', () => {
-                // Handle WebSocket disconnection
-                const webSocketWithId = (Array.from(this.clients[userId]) as WebSocketWithId[]).find((a) => a.socket == socket);
-                if (webSocketWithId) {
-                    this.clients[userId].delete(webSocketWithId);
-                }
-                console.log(`${webSocketsLogPrefix} ${usersDict[userId]?.["username"]} (Client #${userId}) disconnected`, this.clients[userId].size > 2 ? `(There are still ${this.clients[userId].size} sessions)` : this.clients[userId].size > 1 ? `(There is still 1 open session)` : "");
-
-                if (tripId && tripId !== "0") {
-                    const webSocketWithId2 = (Array.from(this.clients[`t${tripId}`]) as WebSocketWithId[]).find((a) => a.socket == socket);
-                    if (webSocketWithId2) {
-                        this.clients[`t${tripId}`].delete(webSocketWithId2);
-                    }
-
-                    console.log(`${webSocketsLogPrefix} ${tripsDict[tripId]?.["name"]} (Trip #${tripId}) disconnected`, this.clients[`t${tripId}`].size > 2 ? `(There are still ${this.clients['t' + tripId].size} sessions)` : this.clients['t' + tripId].size > 1 ? `(There is still 1 open session)` : "");
-                }
-            });
-        });
-
-        console.log(`${webSocketsLogPrefix} WebSocket gateway initialized`);
-    }
-
-    /***
-     *
-     * @param message - the message we want to send to the clients
-     * @param userId - the relevant user id we want to update
-     * @param initiatedByClientId - the id of the client that initiated this update. (to be able to display different messages to the initiator / other clients)
-     *
-     */
-    send(message: string, userId: number|string, initiatedByClientId?: string): void {
-
-        // todo: think of a way to send sockets when 2 different users are on the same trip
-
-        if (userId.toString().startsWith("t")){
-            console.log(`${webSocketsLogPrefix} Sending Message to Trip #${userId}, to ${this.clients[userId]?.size ?? 0} sessions.`)
-        } else {
-            console.log(`${webSocketsLogPrefix} Sending Message to user #${userId}, to ${this.clients[userId]?.size ?? 0} sessions.`)
+        if (tripId && tripId !== '0') {
+          const ws2 = Array.from(this.clients[`t${tripId}`] || []).find((a) => a.socket == socket);
+          if (ws2) this.clients[`t${tripId}`].delete(ws2);
         }
+      });
+    });
 
-        // Send a message to all connected clients
-        this.clients[userId]?.forEach(client => {
-            client.socket.send(JSON.stringify({ message, initiatedByClientId }));
-        });
-    }
+    console.log(`${webSocketsLogPrefix} WebSocket gateway initialized`);
+  }
+
+  send(message: string, userId: number | string, initiatedByClientId?: string): void {
+    const set = this.clients[userId];
+    if (!set) return;
+
+    console.log(`${webSocketsLogPrefix} Sending Message to ${userId}, sessions: ${set.size}`);
+    set.forEach((client) => {
+      client.socket.send(JSON.stringify({ message, initiatedByClientId }));
+    });
+  }
 }
